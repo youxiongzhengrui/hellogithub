13e44846e2e82d4b69e80c51296f52101bd1ae55
youxiongzhengrui <1058561676@qq.com>
2017-05-18 15:30:22 +0800

    third change

--- HelloGithub/HelloGithub/ViewController.m
+++ HelloGithub/HelloGithub/ViewController.m
@@ -17,6 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    UIView *bottomLine = [UIView new];
+//    bottomLine.backgroundColor = [UIColor lightGrayColor];
+//    [_searchView addSubview:bottomLine];
+//    [bottomLine mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.right.left.bottom.equalTo(_searchView);
+//        make.height.mas_equalTo((1.0 / [UIScreen mainScreen].scale));
+//    }];
+//    
+//    
+//    _tableView = [[UITableView alloc] init];
+//    [self.view addSubview:_tableView];
+//    [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.right.bottom.equalTo(self.view);
+//        make.top.equalTo(_searchView.mas_bottom);
+//    }];
+//    _tableView.tableFooterView = [UIView new];
+//    _tableView.delegate = self;
+//    _tableView.dataSource = self;
+//    _tableView.mj_footer=[MJRefreshAutoNormalFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMore)];
+//    _tableView.mj_footer.automaticallyHidden = NO;
+//    _tableView.mj_footer.hidden = YES;
+//    [_txtSearch becomeFirstResponder];
 }
 
 
